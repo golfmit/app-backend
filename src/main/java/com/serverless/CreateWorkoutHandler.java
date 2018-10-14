8a549a1d8347494b84165d549dfaed1f63bc99f3
Radim Pavlicek <radim.pavlicek@laurinka.com>
2018-10-14 11:02:42 +0200

    user workout object as REST parameter

--- src/main/java/com/serverless/CreateWorkoutHandler.java
+++ src/main/java/com/serverless/CreateWorkoutHandler.java
@@ -2,34 +2,27 @@ package com.serverless;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.serverless.dal.Exercise;
 import com.serverless.dal.Workout;
 import org.apache.log4j.Logger;
 
 import java.util.Collections;
-import java.util.Map;
 
-public class CreateWorkoutHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+@SuppressWarnings("unused")
+public class CreateWorkoutHandler implements RequestHandler<Workout, ApiGatewayResponse> {
 
 	private final Logger logger = Logger.getLogger(this.getClass());
 
 	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+	public ApiGatewayResponse handleRequest(Workout input, Context context) {
 
       try {
-          // get the 'body' from input
-          JsonNode body = new ObjectMapper().readTree((String) input.get("body"));
-
           // create the Exercise object for post
           Workout workout = new Workout();
-          // workout.setId(body.get("id").asText());
-          workout.setName(body.get("name").asText());
-		  workout.setGroup(body.get("group").asText());
-          workout.setUser(body.get("user").asText());
-		  workout.setResult(body.get("result").asText());
-          workout.setDate(body.get("date").asText());
+          workout.setName(input.getName());
+		  workout.setGroup(input.getGroup());
+          workout.setUser(input.getUser());
+		  workout.setResult(input.getResult());
+          workout.setDate(input.getDate());
           workout.save(workout);
 
           // send the response back
